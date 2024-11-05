ba58f2846fdbbffc7e3985feb7792733cfe0509f
David Guedes <d2vid.guedes@gmail.com>
2024-11-05 09:20:55 -0300

    fix:ajuste

--- server.js
+++ server.js
@@ -10,16 +10,14 @@ const server = http.createServer(app);
 app.use(cors({ 
     origin: 'https://chat-websocket-angular.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    allowedHeaders: ['Content-Type', 'Authorization']
  }));
 
 const io = new Server(server, {
     cors: { 
         origin: 'https://chat-websocket-angular.vercel.app',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: true,
+        allowedHeaders: ['Content-Type', 'Authorization']
     }
 })
 
